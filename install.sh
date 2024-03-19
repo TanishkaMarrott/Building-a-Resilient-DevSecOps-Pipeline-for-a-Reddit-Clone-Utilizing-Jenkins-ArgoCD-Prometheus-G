390879e31b84493d1b8eb0ae3beed96b124518d2
Tanishka Marrott <78227704+TanishkaMarrott@users.noreply.github.com>
2024-03-19 11:09:08 +0530

    updated install.sh

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update -y
+
+# Install Temurin JDK
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print $2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+sudo apt update -y
+sudo apt install temurin-17-jdk -y
+/usr/bin/java --version
+
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+# Install Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
+
+# Add Jenkins user to Docker group and restart Jenkins
+sudo usermod -aG docker jenkins
+sudo systemctl restart jenkins
+
+# Install Python3 and pip
+sudo apt install python3-pip -y
+
+# Install TruffleHog
+pip3 install trufflehog
+
+# Install tfsec
+wget https://github.com/aquasecurity/tfsec/releases/download/v1.15.0/tfsec-linux-amd64
+chmod +x tfsec-linux-amd64
+sudo mv tfsec-linux-amd64 /usr/local/bin/tfsec
+
+echo "Installation completed! Jenkins, Docker, SonarQube, Trivy, TruffleHog, and tfsec are installed."
